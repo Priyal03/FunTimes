98d35583df5f5aeb2d451ee59f7bc3aeca937788
PriyalPatel <priyalpatel94@gmail.com>
2022-05-10 21:45:58 -0500

    Time: 8 ms (70.97%), Space: 45 MB (43.20%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -0,0 +1,32 @@
+class Solution {
+    //chotu
+    
+    //time - O(n)
+    //space - O(m,n) for hashmap 
+    public int lengthOfLongestSubstring(String s) {
+        
+        Map<Character,Integer> store = new HashMap<Character,Integer>();//to store the current window
+        
+        int left = 0, right = 0, n = s.length(), max=0;
+        
+        while(right<n){
+           
+            char curr = s.charAt(right);//take the next element
+            
+            if(store.containsKey(curr)){
+                //if it contains the dup element, its time to shrink the window by increasing the left pointer.
+                
+                left = Math.max(store.get(curr),left);
+            }
+            //\U0001f495\U0001f60a☺️\U0001f602❤️\U0001f60f\U0001f60d
+            
+            max = Math.max(right-left+1, max);
+            
+            store.put(curr,right+1);//finally add the curr element
+            right++;//and increase the next pointer
+            
+        }
+        
+        return max;
+    }
+}
