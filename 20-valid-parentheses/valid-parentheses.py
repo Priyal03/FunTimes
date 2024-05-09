00696e53ac82d4fe9de533c4a1e66ab0528fa334
Priyal <priyalpatel94@gmail.com>
2024-05-09 16:50:46 -0700

    Time: 30 ms (87.78%) | Memory: 16.6 MB (22.81%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,13 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
 
+        if len(s)%2==1:
+            return False
+            
         for c in s:
             if c=='{' or c=='(' or c=='[':
                 stack.append(c)
 
             elif len(stack)==0:
                 return False
-                
+
             else:
                 popped = stack.pop()
 
