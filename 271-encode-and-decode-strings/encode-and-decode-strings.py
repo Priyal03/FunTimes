27a7d23cef3ba0e0b271700624f08349de11a791
Priyal <priyalpatel94@gmail.com>
2024-05-17 17:15:15 -0700

    Time: 62 ms (84.28%) | Memory: 17 MB (21.62%) - LeetSync

---
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -0,0 +1,28 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        encoded = ''
+        for s in strs:
+            encoded += str(len(s))+'/;'+s
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        decoded = []
+        i=0
+        while i < (len(s)):
+            start = s.find('/;',i)
+            length = int(s[i:start])
+            start+=2
+            end = start+length
+            curr = s[start:end]
+            decoded.append(curr)
+            i=end
+        return decoded
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
