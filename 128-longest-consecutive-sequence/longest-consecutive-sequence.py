967caba1b59918d53a8fcc31efab043fdbd5a11c
Priyal <priyalpatel94@gmail.com>
2024-05-21 17:53:28 -0700

    Time: 4368 ms (19.20%) | Memory: 31.8 MB (51.47%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,19 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
         maxseq = 0
-        nums.sort()
-        current=1
-        for i in range(1,len(nums)):
-            
-            if (nums[i]-1)==nums[i-1]:
-                current+=1
-            elif nums[i]==nums[i-1]:
-                #do nothing because we are trying to find longest distinct characters.
-                continue
-            else:
-                maxseq = max(maxseq,current)
+        hset = set(nums)
+
+        for val in nums:
+            if val-1 not in hset:
+                next = val+1
                 current=1
 
-        return max(maxseq,current)
+                while next in hset:
+                    next+=1
+                    current+=1
+                
+                maxseq = max(maxseq,current)
+
+        return maxseq
