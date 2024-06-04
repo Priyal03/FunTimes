bd6acef1b79e54542f643710906de4d1812533eb
Priyal Patel <priyalpatel94@gmail.com>
2024-06-03 22:28:41 -0700

    Time: 1491 ms (5.05%) | Memory: 31.1 MB (14.74%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,13 +1,28 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxsum = nums[0]
-        windowsum = nums[0]
 
-        for right in range(1, len(nums)):
-            windowsum += nums[right]
-            windowsum = max(
-                windowsum, nums[right]
-            )  # take current element if sum becomes lesser than that.
-            maxsum = max(maxsum, windowsum)
+        def divideandconcur(nums, left, right):
+            if left > right:
+                return -math.inf
 
-        return maxsum
+            mid = (left + right) // 2
+
+            leftmax = rightmax = currsum = 0
+
+            for i in range(mid - 1, left - 1, -1):
+                currsum += nums[i]
+                leftmax = max(leftmax, currsum)
+
+            currsum = 0
+            for i in range(mid + 1, right + 1):
+                currsum += nums[i]
+                rightmax = max(rightmax, currsum)
+
+            currsum = rightmax + leftmax + nums[mid]
+
+            leftsum = divideandconcur(nums, left, mid - 1)
+            rightsum = divideandconcur(nums, mid + 1, right)
+
+            return max(leftsum, rightsum, currsum)
+
+        return divideandconcur(nums, 0, len(nums) - 1)
