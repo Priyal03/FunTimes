9af25d627b4fb42593913c8ae49b77309d496919
Priyal Patel <priyalpatel94@gmail.com>
2024-06-29 19:16:28 -0700

    Time: 85 ms (84.13%), Space: 18.1 MB (95.29%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,22 +1,21 @@
-from collections import deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        matrix = [[] for _ in range(numCourses)]
-        dependencyList = [0] * (numCourses)
-        ans = []
+        adjList = [[] for _ in range(numCourses)]
+        dependencyList = [0]*numCourses
+        result = []
         for course, prereq in prerequisites:
-            matrix[prereq].append(course)
-            dependencyList[course] += 1
-
-        queue = deque(
-            [course for course, count in enumerate(dependencyList) if count == 0]
-        )
+            dependencyList[course]+=1
+            adjList[prereq].append(course)
+        queue=deque()
+        for i in range(numCourses):
+            if dependencyList[i]==0:
+                queue.append(i)
         while queue:
-            course = queue.popleft()
-            ans.append(course)
-            for neighbors in matrix[course]:
-                dependencyList[neighbors] -= 1
-                if dependencyList[neighbors] == 0:
-                    queue.append(neighbors)
-
-        return (ans if len(ans) == numCourses else [])
+            node = queue.pop()
+            result.append(node)
+            for x in adjList[node]:
+                dependencyList[x]-=1
+                if dependencyList[x]==0:
+                    queue.append(x)
+                    
+        return result if len(result)==numCourses else []
