c4505ff8c2d87c1726dc797e95ad89abc48f036c
Priyal Patel <priyalpatel94@gmail.com>
2024-07-01 23:20:37 -0700

    Time: 87 ms (73.46%) | Memory: 18.4 MB (42.65%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,22 +1,26 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        adjmatrix = {node: [] for node in range(n)}
-        count = 0
+        adjList = {i: [] for i in range(n)}
         for x, y in edges:
-            adjmatrix[x].append(y)
-            adjmatrix[y].append(x)
+            adjList[x].append(y)
+            adjList[y].append(x)
 
         visited = set()
 
-        def dfs(node):
-            visited.add(node)
-            for neighbor in adjmatrix[node]:
-                if neighbor not in visited:
-                    dfs(neighbor)
+        def traverse(start):
+            if start in visited:
+                return
+            visited.add(start)
+            for x in adjList[start]:
+                traverse(x)
 
+        count = 0
         for x in range(n):
             if x not in visited:
-                dfs(x)
+                traverse(x)
                 count += 1
 
         return count
+
+        #O(E+V) as adjlist for E and then dfs on V
+        #O(E+V) for space as adjlist(E) and visited(V) sizes
