fd0c715ec13c64d5ef6aefb445fced71f6a6d8ef
Priyal Patel <priyalpatel94@gmail.com>
2024-07-21 18:46:04 -0700

    Time: 334 ms (98.2%), Space: 16.9 MB (75.01%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,19 +2,15 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        self.perimeter=0
-
-        def removeCommonWall(row, col):
-            if row<0 or col<0 or row>=m or col >= n or grid[row][col]==0:
-                return
-            self.perimeter-=1
+        perimeter=0
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==1:
-                    self.perimeter+=4
-                    removeCommonWall(i-1,j)
-                    removeCommonWall(i+1,j)
-                    removeCommonWall(i,j+1)
-                    removeCommonWall(i,j-1)
-        return self.perimeter
+                    perimeter+=4
+                    if i>0 and grid[i-1][j]==1:
+                        perimeter-=2
+                    if j>0 and grid[i][j-1]==1:
+                        perimeter-=2
+                    
+        return perimeter
