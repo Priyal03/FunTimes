09e6e178b8c33d39fe997f5097f7c4d18991ddf3
Priyal Patel <priyalpatel94@gmail.com>
2024-07-24 22:34:23 -0700

    Time: 144 ms (64.88%), Space: 17 MB (58.7%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,16 +1,25 @@
-class Solution(object):
-    def isBipartite(self, graph):
-        color = {}
-        for node in range(len(graph)):
-            if node not in color:
-                stack = [node]
-                color[node] = 0
-                while stack:
-                    node = stack.pop()
-                    for nei in graph[node]:
-                        if nei not in color:
-                            stack.append(nei)
-                            color[nei] = color[node] ^ 1
-                        elif color[nei] == color[node]:
-                            return False
-        return True
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        visited = set()
+        colored = {}
+        vertices = len(graph)
+
+        for i in range(vertices):
+            if i not in visited:
+                if self.dfs(i , graph , colored , 0 , visited) == 0:
+                    return 0
+        
+        return 1
+    
+    def dfs(self , node , graph , colored , color , visited):
+        visited.add(node)
+        colored[node] = color
+        
+        for nei in graph[node]:
+            if nei not in visited:
+                if self.dfs(nei , graph , colored , not color , visited) == 0:
+                    return 0
+            elif colored[nei] == color:
+                return 0
+        
+        return 1
