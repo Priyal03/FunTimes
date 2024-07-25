fe808876aafd65f60d8d2f075c246c69f8664ef7
Priyal Patel <priyalpatel94@gmail.com>
2024-07-24 22:33:41 -0700

    Time: 153 ms (22.55%), Space: 17 MB (58.7%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,17 +1,16 @@
-class Solution:
-    def isBipartite(self, graph: List[List[int]]) -> bool:
-        seen = [0] * len(graph)
-
-        for i in range(len(graph)):
-            if seen[i] == 0:
-                queue = deque([i])
-                seen[i] = -1
-                while queue:
-                    node = queue.popleft()
-                    for x in graph[node]:
-                        if seen[x] == 0:
-                            seen[x] = -1 * seen[node]
-                            queue.append(x)
-                        elif seen[x] == seen[node]:
+class Solution(object):
+    def isBipartite(self, graph):
+        color = {}
+        for node in range(len(graph)):
+            if node not in color:
+                stack = [node]
+                color[node] = 0
+                while stack:
+                    node = stack.pop()
+                    for nei in graph[node]:
+                        if nei not in color:
+                            stack.append(nei)
+                            color[nei] = color[node] ^ 1
+                        elif color[nei] == color[node]:
                             return False
         return True
