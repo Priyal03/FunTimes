573ec86c879ef571cc24b38c376f870a674222c9
Priyal Patel <priyalpatel94@gmail.com>
2024-07-27 14:59:50 -0700

    Time: 96 ms (23.4%), Space: 22.1 MB (5.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,15 +4,16 @@ class Solution:
         
         occurrences = Counter(nums)#take count of each digit.
         
-        heap=[]#create heap to maintain the max element at the top
+        bucket=[[] for _ in range (len(nums)+1)] #bucket create
         for digit, count in occurrences.items():
-            heapq.heappush(heap, (-count, digit))#heapify each element.
+            bucket[count].append(digit)
 
         res=[]
-        #traverse until k elements
-        for _ in range(k):
-            if heap:
-                pair = heappop(heap)#manage heapify after each pop operation
-                res.append(pair[1])
+        #traverse from highest and until k 
+        for i in range(len(bucket)-1,-1,-1):
+            while len(bucket[i])>0 and k>0:
+                
+                res.append(bucket[i].pop())
+                k-=1
             
         return res
