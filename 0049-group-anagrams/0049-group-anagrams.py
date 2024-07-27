0d22383b324cd688c20b7df85ed8ce311a9e1db7
Priyal Patel <priyalpatel94@gmail.com>
2024-07-26 17:17:00 -0700

    Time: 100 ms (21.71%), Space: 21.5 MB (27.24%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #["eat","tea","tan","ate","nat","bat"]
+        anagramStore=defaultdict(list)
+        for word in strs:
+            count=[0]*26
+            for i in word:
+                count[ord(i)-ord('a')]+=1
+            anagramStore[tuple(count)].append(word)
+        return list(anagramStore.values())
