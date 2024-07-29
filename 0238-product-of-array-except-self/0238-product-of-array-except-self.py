4a554b9897e5f2d4d0a62732bb765521c049c649
Priyal Patel <priyalpatel94@gmail.com>
2024-07-28 17:32:51 -0700

    Time: 262 ms (79.21%), Space: 25.7 MB (75.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        ans=[0]*l
-        ans[0]=1
-        r=1
-        for i in range(1,l):
-            ans[i]=ans[i-1]*nums[i-1]
+        n =len(nums)
+        result=[1]*n
 
-        for i in range(l-1,-1,-1):
-            ans[i]=ans[i]*r
-            r *= nums[i]
+        for i in range(1,n):
+            result[i]=nums[i-1]*result[i-1]
 
-        return ans        
+        multiplier=1
+        for i in range(n-1,-1,-1):
+            #multiplier variable will carry multiplication from right side. 
+            result[i]*=multiplier
+            #and it will keep multiplying as it progresses from right side to the left side
+            multiplier*=nums[i]
+
+        return result
