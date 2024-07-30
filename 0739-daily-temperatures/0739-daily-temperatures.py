769dbb08ba41b7441ca43cb25e3b56b993eb0a2d
Priyal Patel <priyalpatel94@gmail.com>
2024-07-30 16:52:09 -0700

    Time: 859 ms (90.4%), Space: 30.8 MB (66.45%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans=[0]*len(temperatures)
         stack=[]
-        for day, temp in enumerate((temperatures)):
-            
-            while stack and temp > temperatures[stack[-1]]:
-                previousday=stack.pop()
-                ans[previousday] = day - previousday
-
-            stack.append(day)
+        ans=[0]*len(temperatures)
+        #set answer only after finding the correct higher temperature, you can't fill up answer while traversing, thats why you must use stack to just add the temps as they come in line and calculate difference only when you find higher temperature.
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]]<temp:
+                popped = stack.pop()
+                ans[popped]=i-popped
+            stack.append(i)
         return ans
