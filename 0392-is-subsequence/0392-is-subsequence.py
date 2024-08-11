015468dc54bf19324b22ac2af2af728ca12b250f
Priyal Patel <priyalpatel94@gmail.com>
2024-08-11 15:18:14 -0700

    Time: 82 ms (6.22%), Space: 17.3 MB (20.04%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,11 +1,21 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        spointer=0
-        tpointer=0
-        while spointer<len(s) and tpointer<len(t):
-            if s[spointer]==t[tpointer]:
-                spointer+=1
-            tpointer+=1
+        slen=len(s)
+        tlen=len(t)
+
+        if slen==0:
+            return True
             
-        return spointer==len(s)
+        dp=[[0]*(tlen+1)for _ in range(slen+1)]
+
+        for col in range(1,tlen+1):
+            for row in range(1,slen+1):
+                if s[row-1]==t[col-1]:
+                    dp[row][col]=dp[row-1][col-1]+1
+                else:
+                    dp[row][col]=max(dp[row][col-1], dp[row-1][col])
+            if dp[slen][col]==slen:
+                return True
+
+        return False
         
