f953e3c3ed3e5ebcdeaa45a02bae4134f2900691
Priyal Patel <priyalpatel94@gmail.com>
2024-08-10 23:24:19 -0700

    Time: 53 ms (91.79%), Space: 23.2 MB (37%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,15 +1,16 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans=[]
-        
-        def countones(x):
-            total=0
-            while x!=0:
-                total+=1
-                x&=(x-1)
-            return total
+        ans=[0]*(n+1)
+        powerof2=1
 
-        for i in range(n+1):
-            ans.append(countones(i))
+        while powerof2 <= n :
+            
+            i=0
+            while i < powerof2 and i+powerof2 <= n :
+                
+                ans[i+powerof2]=ans[i]+1
+                i+=1
+            
+            powerof2<<=1 
 
         return ans
