74531a447ca5a981782da24fd8c0d05e5a5aabe0
Priyal Patel <priyalpatel94@gmail.com>
2024-08-15 20:47:53 -0700

    Time: 52 ms (12.33%), Space: 18.2 MB (5.07%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,15 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapping = {}
-        tmap = {}
+        def transform(input:str)->str:
+            indexmap={}
+            ans=[]
+            for i,c in enumerate(input):
+                if c not in indexmap:
+                    indexmap[c]=i
+                ans.append(indexmap[c])
+            return ''.join(str(ans))
         
-        for i in range(len(s)):
-            if s[i] in mapping and mapping[s[i]]!=t[i]:
-                return False
-            mapping[s[i]]=t[i]
-
-            if t[i] in tmap and tmap[t[i]]!=s[i]:
-                return False
-            tmap[t[i]]=s[i]
-        
-        return len(mapping)==len(tmap)
+        return transform(s)==transform(t)
