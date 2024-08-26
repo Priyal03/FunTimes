82e6bd9a73135ea4bb1fe60f0f541f514c18ebd2
Priyal Patel <priyalpatel94@gmail.com>
2024-08-26 00:08:48 -0700

    Time: 117 ms (85.24%), Space: 20.6 MB (51.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,13 +4,13 @@ class Solution:
         ans=[sort[0]]
         
         for i in range(1,len(sort)):
-            
-            if ans[-1][1]>=sort[i][0]:
-                if ans[-1][1]<sort[i][1]:
+            #current element's start must be smaller than last element's end
+            if ans[-1][1]>=sort[i][0]: 
+                #last element's end must be smaller than current's
+                if ans[-1][1]<sort[i][1]: 
                     ans[-1][1]=sort[i][1]
-
+                #else skip it as that interval is redundant
             else:
-                ans.append(sort[i])
-            
+                ans.append(sort[i])   
 
         return ans
