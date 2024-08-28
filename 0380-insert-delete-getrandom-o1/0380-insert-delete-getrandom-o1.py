313188405d2898c61816ae03e0d26e3498e0763f
Priyal Patel <priyalpatel94@gmail.com>
2024-08-27 23:06:28 -0700

    Time: 255 ms (73.01%), Space: 63.9 MB (38.08%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,26 +1,34 @@
 class RandomizedSet:
-
+#take map to insert and delete in O(1) time but take array to get random in O(1) time as we can't use random lib for hashmap.
     def __init__(self):
-        self.random_map=set()
+        self.random_map={}
+        self.array=[]
 
     def insert(self, val: int) -> bool:
         if val in self.random_map:
             return False
-        self.random_map.add(val)
+
+        self.array.append(val)
+        self.random_map[val]=len(self.array)-1
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.random_map:
             return False
-        self.random_map.remove(val)
+        
+        index=self.random_map[val]
+        lastelement=self.array[-1]
+
+        self.array[index]=lastelement
+        self.random_map[lastelement]=index
+
+        del self.random_map[val]
+        self.array.pop()
         return True        
 
     def getRandom(self) -> int:
-        randomlist=list(self.random_map)
-        randomInt = random.randint(0,len(randomlist)-1)
-        return randomlist[randomInt]
-        # return random.choice(list(self.random_map))
-
+        return random.choice(self.array)
+        
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
