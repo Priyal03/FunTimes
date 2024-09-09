0dc91449615e44f8233552f0c261dc71c50f8efb
Priyal Patel <priyalpatel94@gmail.com>
2024-09-08 17:55:58 -0700

    Time: 704 ms (29.1%), Space: 27.7 MB (88.95%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.py
+++ 0565-array-nesting/0565-array-nesting.py
@@ -4,13 +4,14 @@ class Solution:
         
         for i in range(len(nums)):
             count=0
-            while i!=float('inf'):
+            while i!=100001:
                 
                 temp=i
                 count+=1
                 
                 i=nums[i]
-                nums[temp]=float('inf')
+                nums[temp]=100001 
+                #float('inf')
 
             ans=max(ans,count-1)
 
