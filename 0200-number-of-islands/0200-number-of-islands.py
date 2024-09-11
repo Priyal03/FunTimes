56b0499a2bcf8cfe0e1c541fc9ac7a7d3ecc1270
Priyal Patel <priyalpatel94@gmail.com>
2024-09-11 13:03:11 -0700

    Time: 232 ms (79.56%), Space: 19 MB (61.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        m = len(grid)
-        n = len(grid[0])
+        total=0
+        rows=len(grid)
+        cols=len(grid[0])
+        if not grid:
+            return 0
+        queue=deque([])
+        directions=[(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1":
+                    queue.append((r,c))
+                    grid[r][c]="0"
+                    while queue:
+                        x,y=queue.popleft()
+                        for d in directions:
+                            nx = x+d[0]
+                            ny= y+d[1]
+                            if 0<=nx<rows and 0<=ny<cols and grid[nx][ny]=="1":
+                                queue.append((nx,ny))
+                                grid[nx][ny]="0"
+                    total+=1
 
-        def dfs(i, j):
-            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == "0":
-                return False
-            if (i, j) in visited:
-                return False
-            else:
-                visited.add((i, j))
-                if grid[i][j] == "1":
-                    dfs(i + 1, j)
-                    dfs(i - 1, j)
-                    dfs(i, j - 1)
-                    dfs(i, j + 1)
-                return True     
-
-        islands = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    if dfs(i, j):
-                        islands += 1
-
-        return islands
+        return total
