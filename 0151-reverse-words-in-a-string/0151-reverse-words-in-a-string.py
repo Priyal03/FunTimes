ef394b06c15a7353f835ef925fef0d3929adfeda
Priyal Patel <priyalpatel94@gmail.com>
2024-09-11 22:56:41 -0700

    Time: 45 ms (9.81%), Space: 16.7 MB (17.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,8 +1,39 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.strip().split(" ")
-        ans=""
-        for i in range(len(words)-1,-1,-1):
-            if words[i]:
-                ans+=(words[i])+(" ")
-        return ans.strip()
+        def trim_spaces(s):
+            n=len(s)
+            left=0
+            right=n-1
+            while left<=right and s[left]==' ':
+                left+=1
+            while left<=right and s[right]==' ':
+                right-=1
+            ans=[]
+            while left<=right:
+                if s[left]!=' ':
+                    ans.append(s[left])
+                elif ans and ans[-1]!=' ':
+                    ans.append(' ')
+                left+=1
+            return ans
+
+        def reverse_list(inputStr,left,right):
+            while left<right:
+                inputStr[left],inputStr[right]=inputStr[right],inputStr[left]
+                left+=1
+                right-=1
+
+        def reverse_each_word(word):
+            n=len(word)
+            start=0
+            while start<n:
+                end=start
+                while end<n and word[end]!=' ':
+                    end+=1
+                reverse_list(word,start,end-1)
+                start=end+1
+
+        trimmed=trim_spaces(s)
+        reverse_list(trimmed,0,len(trimmed)-1)
+        reverse_each_word(trimmed)
+        return ''.join(trimmed)
