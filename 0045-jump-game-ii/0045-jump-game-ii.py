249415caaf41a39d70c725d073d31c7059c4ceb9
Priyal Patel <priyalpatel94@gmail.com>
2024-09-12 12:45:45 -0700

    Time: 110 ms (42.22%), Space: 17.7 MB (14.97%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        count = 0
-        n = len(nums)
-        currJump = maxJump = 0
-        for i in range(n - 1):
-            # keep updating the max jump you can make
-            maxJump = max(maxJump, i + nums[i])
-            # increase the counter only when your index reaches maxpossiblejump for current maxJump value, and now make that variable ready for next count by storing maxJump
-            if i == currJump:
-                count += 1
-                currJump = maxJump
-        return count
+        endPointer=0
+        farthestReach=0
+        steps=0
+
+        for i in range(len(nums)-1):
+            
+            farthestReach=max(farthestReach,nums[i]+i)
+
+            if i==endPointer:
+                endPointer=farthestReach
+                steps+=1
+
+        return steps
