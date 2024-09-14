ce80eda5b6cf7478db744cb12c9a4fcdf2b400e0
Priyal Patel <priyalpatel94@gmail.com>
2024-09-13 17:24:51 -0700

    Time: 2085 ms (5%), Space: 78.2 MB (49.71%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -2,31 +2,27 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity=capacity
-        self.cache={}
-        self.order=deque()
+        self.hashmap={}
+        self.queue=deque()
 
     def get(self, key: int) -> int:
-        
-        if key not in self.cache:
+        if key not in self.hashmap:
             return -1
-
-        self.order.remove(key)
-        self.order.append(key)
-        return self.cache[key]
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.hashmap[key]
 
     def put(self, key: int, value: int) -> None:
-        
-        if key in self.cache:
-            self.order.remove(key)
-        self.cache[key]=value
-        self.order.append(key)
-        # print(self.cache)
+        if key in self.hashmap:
+            self.queue.remove(key)
 
-        if len(self.cache) > self.capacity:
-            leastUsed = self.order.popleft()
-            del self.cache[leastUsed]
-
+        self.hashmap[key]=value
+        self.queue.append(key)
         
+        if len(self.hashmap)>self.capacity:
+            leastUsedKey = self.queue.popleft()
+            del self.hashmap[leastUsedKey]
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
