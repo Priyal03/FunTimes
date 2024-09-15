32e5760a266e5bf719d42b3a47e5d33670d10542
Priyal Patel <priyalpatel94@gmail.com>
2024-09-14 17:37:09 -0700

    Time: 42 ms (97.3%), Space: 16.6 MB (40.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,14 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        chars=[None]*128
-        l=r=0
-        ans=0
-        while r<len(s):
-            right=s[r]
-            index=chars[ord(right)]
-            if index is not None and l<=index<r:
-                l=index+1
-            ans=max(ans,r-l+1)
-            chars[ord(right)]=r #key,value pair of ASCII value of 'a' and then last occured index pos of that in string.
-            r+=1
-        return ans
+        left = 0
+        maxlen = 0
+        occur = [None]*128
+
+        for right in range(len(s)):
+
+            if occur[ord(s[right])] is not None: #if you find again.
+            
+                left = max(occur[ord(s[right])] + 1, left)#update the left bound value.
+
+            maxlen = max(maxlen, right - left + 1)
+
+            occur[ord(s[right])] = right #save everytime.
+
+        return maxlen
