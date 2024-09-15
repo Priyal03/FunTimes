396f1cf2cccd50b8d2a88b0a30a8cc50788f8303
Priyal Patel <priyalpatel94@gmail.com>
2024-09-15 14:59:34 -0700

    Time: 78 ms (80.53%), Space: 17.9 MB (8.28%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,19 +1,22 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack=[]
-        for curr in asteroids:
-            isBig = True
-            while stack and stack[-1]>0 and curr < 0:
-                if stack[-1]<abs(curr):
-                    stack.pop()
-                elif stack[-1]==abs(curr):
-                    stack.pop()
-                    isBig=False
-                    break
-                else:
-                    isBig=False
-                    break
-            if isBig:
-                stack.append(curr)
-        return stack
-                
+        ans=[]
+# We don't really need to add negative asteroids into stack.
+# Consider stack as a barrier of positive asteroids that negative should pass
+        for asteroid in asteroids:
+            
+            if asteroid > 0:
+                stack.append(asteroid)
+            else:
+                while stack and abs(asteroid) > stack[-1] :
+                    stack.pop() #if negative is higher we keep removing positives from stack
+
+                if len(stack)==0: 
+                    ans.append(asteroid)#if negative beats all positives
+
+                elif stack[-1]==abs(asteroid):
+                        stack.pop()
+
+        ans+=stack
+        return ans
