82881eb34b3e94c1ab8493e0049ad413fa8ed8cd
Priyal Patel <priyalpatel94@gmail.com>
2024-09-17 13:48:32 -0700

    Time: 68 ms (85.85%), Space: 19.9 MB (90.4%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,12 +1,16 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        minHeap = []
-        heapq.heappush(minHeap, intervals[0][1])
+        
+        minheap=[]
+        heapq.heappush(minheap, intervals[0][1])
 
-        for interval in intervals[1:]:
-            if interval[0] >= minHeap[0]:
-                heapq.heappop(minHeap)
+        for i in range(1,len(intervals)):
+            minimum = minheap[0]
+            #remove whenever starttime is greater than minimum top element as meeting room can be available after that time.
+            if intervals[i][0]>=minimum:
+                heapq.heappop(minheap)
+            #keep pushing it in heap for every new intervals.    
+            heapq.heappush(minheap, intervals[i][1])
 
-            heapq.heappush(minHeap, interval[1])
-        return len(minHeap)
+        return len(minheap)
