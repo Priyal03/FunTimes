c8847517bac0a5930fb6205ee5163fbbb6a82d97
Priyal Patel <priyalpatel94@gmail.com>
2024-09-19 20:11:52 -0700

    Time: 79 ms (94.14%), Space: 21 MB (87.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,18 +2,8 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        occurrences = Counter(nums)#take count of each digit.
+        hmap = Counter(nums)#take count of each digit.
         
-        bucket=[[] for _ in range (len(nums)+1)] #bucket create
-        for digit, count in occurrences.items():
-            bucket[count].append(digit)
+        result = sorted(hmap, key=hmap.get, reverse=True)
 
-        res=[]
-        #traverse from highest and until k 
-        for i in range(len(bucket)-1,-1,-1):
-            while len(bucket[i])>0 and k>0:
-                
-                res.append(bucket[i].pop())
-                k-=1
-            
-        return res
+        return result[:k]
