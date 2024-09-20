cfa82de6172f0d10278fbc43ffaebf9ed113af92
Priyal Patel <priyalpatel94@gmail.com>
2024-09-19 19:13:00 -0700

    Time: 42 ms (92.6%), Space: 20.2 MB (93.01%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,17 +6,34 @@
 #         self.right = None
 
 class Solution:
+# Parent Pointers:
+# We use a dictionary parent to store the parent of each node. Initially, we store the root with None as its parent.
+# We use a stack to traverse the tree iteratively (similar to depth-first search). For each node, we record its parent.
+# Finding Ancestors:
+# Once we have the parent pointers, we trace all ancestors of p (starting from p and moving upwards using the parent dictionary) and store them in a set ancestors.
+# Finding LCA:
+# Then, we move upwards from q using the parent dictionary. The first ancestor of q that is also in the ancestors set (which contains the ancestors of p) is the lowest common ancestor.
+
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root is None:
-            return None
+        parent={root:None}
+        stack=[root]
 
-        if root == p or root==q:
-            return root
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left]=node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right]=node
+                stack.append(node.right)
 
-        left_lca = self.lowestCommonAncestor(root.left,p,q)
-        right_lca = self.lowestCommonAncestor(root.right,p,q)
+        ancestors = set()
 
-        if left_lca and right_lca: #if both has a value means, p and q are in different subtrees
-            return root
+        while p:
+            ancestors.add(p)
+            p=parent[p]
 
-        return left_lca if left_lca else right_lca # if one of them is true means, p and q exists in same subtree
+        while q not in ancestors:
+            q=parent[q]
+        
+        return q
