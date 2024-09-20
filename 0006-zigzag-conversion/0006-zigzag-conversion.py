88f963a92850d43e75b507406e2c9ddb2ae29d01
Priyal Patel <priyalpatel94@gmail.com>
2024-09-20 12:31:01 -0700

    Time: 48 ms (73.33%), Space: 16.8 MB (30.41%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,24 +1,22 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
+        if numRows==1:
             return s
 
-        ans = []
-        n = len(s)
-        offset = (numRows-1)*2
-        
-        for i in range(numRows):
-            index = i
-            while index < n:
-                ans.append(s[index])
+        n=len(s)
+        ans=[]
+        sectionLength = (numRows-1)*2
 
-                if i != 0 and i != (numRows - 1):
-                    distanceBetween = offset - 2 * i
-                    insideIndex = index + distanceBetween
+        for currRow in range(numRows):
+            index=currRow
+            while index<n:
+                ans.append(s[index]) # this is for all vertical characters after whole section.
+                if currRow!=0 and currRow!=(numRows-1):
+                    jump = sectionLength-2*currRow #(jump is only for the diagonal patterned characters)
+                    middleIndex = index+jump
+                    if middleIndex<n:
+                        ans.append(s[middleIndex])
 
-                    if insideIndex < n:
-                        ans.append(s[insideIndex])
-
-                index += offset
-
-        return "".join(ans)
+                index+=sectionLength
+                
+        return ''.join(ans)
