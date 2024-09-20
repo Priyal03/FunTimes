3e197fabf1b4ab80990b5162270b4932aa86ccbe
Priyal Patel <priyalpatel94@gmail.com>
2024-09-20 12:10:07 -0700

    Time: 21 ms (99.36%), Space: 16.5 MB (32.14%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,23 +1,24 @@
 class Solution:
-#     When we hit an opening bracket [, we push the current_str and current_num to the stack and reset them to start processing the new substring.
-# When we hit a closing bracket ], we pop the stack to retrieve the previous string and the number k, then repeat the current string k times and append it to the previous string.
+    index=0
     def decodeString(self, s: str) -> str:
-        stack=[]
-        digit=0
-        result=""
-
-        for char in s:
-            if char.isdigit():
-                digit = digit*10 + int(char)
-            elif char.isalpha():
-                result+=char
-            
-            elif char=='[':
-                stack.append((result,digit))
-                result=""
+        result=[]
+        
+        while self.index<len(s):
+            if s[self.index]==']':
+                break
+            if s[self.index].isalpha():
+                result.append(s[self.index])
+                self.index+=1
+            else:
                 digit=0
-            elif char==']':
-                prev_str, num = stack.pop()
-                result = prev_str+result*num
-            
-        return result
+                while self.index<len(s) and s[self.index].isdigit():
+                    digit=digit*10+int(s[self.index])
+                    self.index+=1
+                self.index+=1
+                decoded=self.decodeString(s)
+                result.append(decoded*digit)
+                self.index+=1
+        
+        return ''.join(result)
+
+        #redo this!!!! 
