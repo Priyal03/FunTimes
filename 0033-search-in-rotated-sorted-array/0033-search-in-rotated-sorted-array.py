95b047af75337fa763fa008a2d89a2f4ff3a01ac
Priyal Patel <priyalpatel94@gmail.com>
2024-09-21 22:25:29 -0700

    Time: 54 ms (5.4%), Space: 17.1 MB (5.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,27 +5,19 @@ class Solution:
         high=n-1
         while low<=high:
             mid=low+(high-low)//2
-            if nums[mid]>nums[-1]:
-                low=mid+1
+            if nums[mid]==target:
+                return mid
+#left subarray of mid pointer is sorted
+            elif nums[mid]>=nums[low]:
+                if target>=nums[low] and target<nums[mid]:
+                    high=mid-1
+                else:
+                    low=mid+1
+#right subaray of mid pointer is sorted.                
             else:
-                high=mid-1
-            
-        inflectionPoint = low
-        degreeOfRotataion = n - inflectionPoint
-
-        low = (degreeOfRotataion + inflectionPoint) % n
-        high = (degreeOfRotataion + inflectionPoint - 1) % n
-
-        while low<=high:
-            
-            mid=low+(high-low)//2
-            newMid = (mid - degreeOfRotataion)%n
-
-            if nums[newMid]==target:
-                return newMid
-            elif nums[newMid]>target:
-                high=mid-1
-            else:
-                low=mid+1
+                if target<=nums[high] and target> nums[mid]:
+                    low=mid+1
+                else:
+                    high=mid-1
 
         return -1
