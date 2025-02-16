2259f26cb1cde9ed3da17c0226acfe85089761eb
Priyal Patel <priyalpatel94@gmail.com>
2025-02-16 15:33:19 +0530

    Time: 59 ms (31.31%), Space: 18 MB (18.96%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,8 +1,27 @@
+from collections import Counter
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1 = ''.join(sorted(s1))
-        for i in range(len(s2) - len(s1) + 1):
-            if s1 == ''.join(sorted(s2[i:i + len(s1)])):
+        
+        if len(s1) > len(s2):
+            return False
+        
+        #take two counters for starters 
+        s1_freq = Counter(s1)
+        s2_freq = Counter(s2[: len(s1)])
+        
+        if s1_freq == s2_freq:
+            return True #only valid case.
+
+        for i in range(len(s1), len(s2)):
+            s2_freq[s2[i - len(s1)]] -= 1 #remove the prev element from counter
+
+            if s2_freq[s2[i - len(s1)]] == 0:
+                del s2_freq[s2[i - len(s1)]] #remove element from the window if counter is zero after decrease.
+
+            s2_freq[s2[i]] += 1 #add current element to the counter
+
+            if s1_freq == s2_freq:
                 return True
+
         return False
-#use sorted strings to finally compare whether permutation is same or not
