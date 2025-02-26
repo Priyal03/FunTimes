f56da6ef2a5c1b66bf9eeb7d9e1749ef62b4b0c2
Priyal Patel <priyalpatel94@gmail.com>
2025-02-26 06:48:39 +0530

    Time: 15 ms (4.12%), Space: 17.8 MB (80.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,7 @@
 class Solution:
-
     def isValid(self, s: str) -> bool:
 
-        stack = []
+        while '()' in s or '[]' in s or '{}' in s:
+            s=s.replace("()","").replace("{}","").replace('[]',"")
 
-        mapping={"(":")","{":"}","[":"]"}
-
-        for chars in s:
-            
-            if chars in mapping:
-                stack.append(chars)
-            
-            else:
-                if not stack: #if stack is empty , then we do not have opening braces
-                    return False
-
-                popped = stack.pop()
-
-                if mapping[popped] !=  chars: #if we can not find opening brace's pair in mapping to current element
-
-                    return False
-                
-        return not stack # if stack is empty then string is valid
+        return s==''
